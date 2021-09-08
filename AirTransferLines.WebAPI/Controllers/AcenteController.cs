7f580c9e0433b3136f10be6d19c4a40f2cef6c16
BilalOzel7 <mbozel07@gmail.com>
2021-09-08 22:53:51 +0300

    WebAPI katmanı eklendi.

--- AirTransferLines.WebAPI/Controllers/AcenteController.cs
+++ AirTransferLines.WebAPI/Controllers/AcenteController.cs
@@ -1,4 +1,5 @@
 ﻿using AirTransferLines.Business.Abstract;
+using AirTransferLines.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -17,6 +18,72 @@ namespace AirTransferLines.WebAPI.Controllers
         public AcenteController(IAcenteService acenteService)
         {
             _acenteService = acenteService;
+        }
+
+        [HttpGet("getall")]
+
+        public IActionResult Get()
+        {
+            
+
+            var result = _acenteService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+
+        }
+
+        [HttpGet("getbyıd")]
+
+        public IActionResult GetById(int id)
+        {
+            var result = _acenteService.GetByID(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Acente acente)
+        {
+
+            var result = _acenteService.Add(acente);
+            var a = result;
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+
+        }
+            [HttpPost("update")]
+            public IActionResult Update(Acente acente)
+            {
+
+                var result = _acenteService.Add(acente);
+                var a = result;
+                if (result.Success)
+                {
+                    return Ok(result);
+                }
+                return BadRequest(result);
+            }
+
+        [HttpPost("delete")]
+        public IActionResult Delete(Acente acente)
+        {
+
+            var result = _acenteService.Add(acente);
+            var a = result;
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
         }
     }
 }
